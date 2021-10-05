6a4ba6ff83ac09e22bfffcff4ca3d6c4f58c488e
rryaboshtan <rrvworkroman@gmail.com>
2021-10-06 02:05:32 +0300

    more simple debounce

--- assets/js/helpers/debounce.js
+++ assets/js/helpers/debounce.js
@@ -1,15 +1,15 @@
-export default function debounce(func, wait, immediate) {
-   let timeout;
-   return function () {
-      const context = this,
-         args = arguments;
-      const later = function () {
-         timeout = null;
-         if (!immediate) func.apply(context, args);
-      };
-      const callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
+export default function debounce(fn, delay = 250) {
+   let timer = null;
+
+   if (!delay) {
+      return fn;
+   }
+
+   return function (...args) {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+         fn.apply(this, args);
+         console.log('Args = ', args);
+      }, delay);
    };
 }
