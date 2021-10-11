a5a7aecb5011519dd739aaf5d73e8cfa03761e31
rryaboshtan <rrvworkroman@gmail.com>
2021-10-11 23:00:00 +0300

    refactor: add renderUser method

--- assets/js/modules/view.js
+++ assets/js/modules/view.js
@@ -18,34 +18,34 @@ export class View {
    }
 
    init() {
-        this.app = document.getElementById('app');
-        this.user = this.createElement('div', 'user');
+      this.app = document.getElementById('app');
+      this.user = this.createElement('div', 'user');
 
-        this.title = this.createElement('h1', 'title');
-        this.title.textContent = 'GitHub Search Users';
+      this.title = this.createElement('h1', 'title');
+      this.title.textContent = 'GitHub Search Users';
 
-        this.searchLine = this.createElement('div', 'search-line');
-        this.searchInput = this.createElement('input', 'search-input');
-        this.searchCounter = this.createElement('span', 'counter');
+      this.searchLine = this.createElement('div', 'search-line');
+      this.searchInput = this.createElement('input', 'search-input');
+      this.searchCounter = this.createElement('span', 'counter');
 
-        this.searchLine.append(this.searchInput);
-        this.searchLine.append(this.searchCounter);
+      this.searchLine.append(this.searchInput);
+      this.searchLine.append(this.searchCounter);
 
-        this.usersWrapper = this.createElement('div', 'users-wrapper');
-        this.usersList = this.createElement('ul', 'users');
-        this.usersWrapper.append(this.usersList);
+      this.usersWrapper = this.createElement('div', 'users-wrapper');
+      this.usersList = this.createElement('ul', 'users');
+      this.usersWrapper.append(this.usersList);
 
-        this.main = this.createElement('div', 'main');
-        this.main.append(this.usersWrapper);
+      this.main = this.createElement('div', 'main');
+      this.main.append(this.usersWrapper);
 
-        this.loadMoreBtn = this.createElement('button', 'btn');
-        this.loadMoreBtn.textContent = 'Загрузить ещё';
-        this.loadMoreBtn.style.display = 'none';
-        this.usersWrapper.append(this.loadMoreBtn);
+      this.loadMoreBtn = this.createElement('button', 'btn');
+      this.loadMoreBtn.textContent = 'Загрузить ещё';
+      this.loadMoreBtn.style.display = 'none';
+      this.usersWrapper.append(this.loadMoreBtn);
 
-        this.app.append(this.title);
-        this.app.append(this.searchLine);
-        this.app.append(this.main);
+      this.app.append(this.title);
+      this.app.append(this.searchLine);
+      this.app.append(this.main);
    }
 
    createElement(elementTag, elementClass) {
@@ -76,18 +76,20 @@ export class View {
             this.user.innerHTML = '';
          }
 
-         this.user.innerHTML = `<img src='${userData.avatar_url}' alt='${userData.login}'>
-                           <h2>${userData.login}</h2>
-                           ${followingList}
-                           ${followersList}
-                           ${reposList}
-                           `;
+         this.renderUser(this.user, userData, followingList, followersList, reposList);
       });
 
       this.main.append(this.user);
    }
 
- 
+   renderUser(user, userData, followingList, followersList, reposList) {
+      user.innerHTML = `<img src='${userData.avatar_url}' alt='${userData.login}'>
+                           <h2>${userData.login}</h2>
+                           ${followingList}
+                           ${followersList}
+                           ${reposList}
+                           `;
+   }
 
    createDataList(list, title) {
       const block = this.createElement('div', 'user-block');
